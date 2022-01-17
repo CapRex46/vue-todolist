0c6a96a39899f1b4b6a92cbd9a02a80df1e22052
agres <ale.agresta46@gmail.com>
2022-01-17 16:12:30 +0100

    added some milestone

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ new Vue ({
             this.temporaryItem=''
         },
         removeItem: function(index){
-            console.log("rimuovo", index)
             this.todo.splice(index, 1)
         }
     } 
