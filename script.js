3d0f8741a604e0164ff937bde6ec3c5fe14fcd9f
agres <ale.agresta46@gmail.com>
2022-01-18 11:43:03 +0100

    fix

--- script.js
+++ script.js
@@ -2,19 +2,38 @@ new Vue ({
     el: '#app',
     data: {
         todo: [
-            "fare i compiti",
-            "fare il bucato",
-            "fare il bucato",
+            {
+                text:"fare i compiti",
+                done: false,
+            },
+
+            {
+                text:"fare il bucato",
+                done: true,
+            },
+
+            {
+                text:"fare il bucato",
+                done: false
+            }            
+            
         ],
         temporaryItem: ''
     },
     methods: {
         addItem: function() {
-            this.todo.push(this.temporaryItem)
-            this.temporaryItem=''
+            // this.todo.push(this.temporaryItem)
+            this.todo.push({
+                text: this.temporaryItem,
+                done: false
+            });
+            this.temporaryItem = ''
         },
         removeItem: function(index){
             this.todo.splice(index, 1)
+        },
+        doneUndone: function (index){
+            this.todo[index].done = !this.todo[index].done
         }
     } 
 });
